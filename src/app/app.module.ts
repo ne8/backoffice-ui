ac0c7e561753a5844a5e598f3f3c86a4291d2812
neop <cpatrut@outlook.com>
2019-02-10 18:07:57 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { OrdersComponent } from './orders/orders.component';
+import { BackofficeRoutesModule } from './routes/routes/routes.module';
+import { LoginComponent } from './user-handler/login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import {MatMenuModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent,
+    OrdersComponent,
+    LoginComponent,
+    MenuComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BackofficeRoutesModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
